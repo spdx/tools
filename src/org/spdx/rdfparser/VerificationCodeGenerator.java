2e69d483b87f86f5bbeb1abea1a4cf76921eb280
Gary O'Neall <gary@sourceauditor.com>
2011-08-24 15:04:08 -0700

    Add implementation of VerificationCodeGenerator and application to generate verification codes

--- src/org/spdx/rdfparser/VerificationCodeGenerator.java
+++ src/org/spdx/rdfparser/VerificationCodeGenerator.java
@@ -17,16 +17,19 @@
 package org.spdx.rdfparser;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 /**
  * Generates a package verification code from a directory of source code.  
  * 
- * A class implementing the IFileChecksumGenerator is supplied as a paramater to the constructor.
+ * A class implementing the IFileChecksumGenerator is supplied as a parameter to the constructor.
  * The method <code>getFileChecksum</code> is called for each file in the directory.  This can
  * be used as a hook to capture all files in the directory and capture the checksum values at 
  * a file level.
@@ -36,6 +39,7 @@ import java.util.Collections;
  */
 public class VerificationCodeGenerator {
 
+	private static final String END_OF_LINE_CHAR = "\n";
 	private IFileChecksumGenerator fileChecksumGenerator;
 	
 	public VerificationCodeGenerator(IFileChecksumGenerator fileChecksumGenerator) {
@@ -46,10 +50,19 @@ public class VerificationCodeGenerator {
 	 * @param sourceDirectory
 	 * @return
 	 * @throws NoSuchAlgorithmException 
+	 * @throws IOException 
 	 */
-	public SpdxPackageVerificationCode generatePackageVerificationCode(File sourceDirectory) throws NoSuchAlgorithmException {
+	public SpdxPackageVerificationCode generatePackageVerificationCode(File sourceDirectory, File[] skippedFiles) throws NoSuchAlgorithmException, IOException {
+		// create a sorted list of file paths
+		TreeSet<String> skippedFilesPath = new TreeSet<String>();
+		String rootOfDirectory = sourceDirectory.getAbsolutePath();
+		int rootLen = rootOfDirectory.length()+1;
+		for (int i = 0; i < skippedFiles.length; i++) {
+			String skippedPath = normalizeFilePath(skippedFiles[i].getAbsolutePath().substring(rootLen));
+			skippedFilesPath.add(skippedPath);
+		}
 		ArrayList<String> fileNameAndChecksums = new ArrayList<String>();
-		collectFileData(sourceDirectory.getAbsolutePath(), sourceDirectory, fileNameAndChecksums);
+		collectFileData(rootOfDirectory, sourceDirectory, fileNameAndChecksums, skippedFilesPath);
 		Collections.sort(fileNameAndChecksums);
 		MessageDigest verificationCodeDigest = MessageDigest.getInstance("SHA-1");
 		for (int i = 0;i < fileNameAndChecksums.size(); i++) {
@@ -57,7 +70,13 @@ public class VerificationCodeGenerator {
 			verificationCodeDigest.update(hashInput);
 		}
 		String value = convertChecksumToString(verificationCodeDigest.digest());
-		SpdxPackageVerificationCode retval = new SpdxPackageVerificationCode(value, new String[0]);
+		String[] skippedFileNames = new String[skippedFilesPath.size()];
+		Iterator<String> iter = skippedFilesPath.iterator();
+		int i = 0;
+		while (iter.hasNext()) {
+			skippedFileNames[i++] = iter.next();
+		}
+		SpdxPackageVerificationCode retval = new SpdxPackageVerificationCode(value, skippedFileNames);
 		return retval;
 	}
 
@@ -66,26 +85,55 @@ public class VerificationCodeGenerator {
 	 * @param prefixForRelative The portion of the filepath which preceeds the relative file path for the archive
 	 * @param sourceDirectory
 	 * @param fileNameAndChecksums
+	 * @throws IOException 
 	 */
 	private void collectFileData(String prefixForRelative, File sourceDirectory,
-			ArrayList<String> fileNameAndChecksums) {
+			ArrayList<String> fileNameAndChecksums, TreeSet<String> skippedFiles) throws IOException {
 		if (!sourceDirectory.isDirectory()) {
 			return;
 		}
 		File[] filesAndDirs = sourceDirectory.listFiles();
 		for (int i = 0; i < filesAndDirs.length; i++) {
 			if (filesAndDirs[i].isDirectory()) {
-				collectFileData(prefixForRelative, filesAndDirs[i], fileNameAndChecksums);
+				collectFileData(prefixForRelative, filesAndDirs[i], fileNameAndChecksums, skippedFiles);
 			} else {
-				String checksumValue = this.fileChecksumGenerator.getFileChecksum(filesAndDirs[i]);
-				String filePath = filesAndDirs[i].getAbsolutePath()
-									.substring(prefixForRelative.length())
-									.replace('\\', '/').trim();
-				fileNameAndChecksums.add(checksumValue+filePath);
+				String filePath = normalizeFilePath(filesAndDirs[i].getAbsolutePath()
+						.substring(prefixForRelative.length()+1));
+				if (!skippedFiles.contains(filePath)) {
+					String checksumValue = this.fileChecksumGenerator.getFileChecksum(filesAndDirs[i]).toLowerCase();
+					fileNameAndChecksums.add(checksumValue+"||"+filePath+END_OF_LINE_CHAR);
+				}
 			}
 		}
 	}
 
+	/**
+	 * Normalizes a file path per the SPDX spec
+	 * @param nonNormalizedFilePath
+	 * @return
+	 */
+	public static String normalizeFilePath(String nonNormalizedFilePath) {
+		String filePath = nonNormalizedFilePath.replace('\\', '/').trim();		
+		if (filePath.contains("../")) {
+			// need to remove these references
+			String[] filePathParts = filePath.split("/");
+			StringBuilder normalizedFilePath = new StringBuilder();
+			for (int j = 0; j < filePathParts.length; j++) {
+				if (filePathParts[j].equals("..")) {
+					// remove these from the filePath
+				} else {
+					if (j > 0) {
+						normalizedFilePath.append("/");
+					}
+					normalizedFilePath.append(filePathParts[j]);
+				}
+			}
+			filePath = normalizedFilePath.toString();
+		}
+		filePath.replace("./", "");
+		filePath = "./" + filePath;
+		return filePath;
+	}
 	/**
 	 * Convert a byte array SHA-1 digest into a 40 character hex string
 	 * @param digest
@@ -101,5 +149,16 @@ public class VerificationCodeGenerator {
 			sb.append(hex);
 		}
 		return sb.toString();
+	}
+	/**
+	 * @param sourceDirectory
+	 * @param skippedFiles
+	 * @return
+	 * @throws NoSuchAlgorithmException 
+	 * @throws IOException 
+	 */
+	public SpdxPackageVerificationCode generatePackageVerificationCode(
+			File sourceDirectory) throws NoSuchAlgorithmException, IOException {
+		return generatePackageVerificationCode(sourceDirectory, new File[0]);
 	}
 }
