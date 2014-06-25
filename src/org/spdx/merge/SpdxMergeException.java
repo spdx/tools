e2fe0716278e051611764132733e6a907407bcec
gling07 <linggang7@gmail.com>
2014-06-25 18:29:12 -0500

    First look of merge tool project. Contiune working on package info and license info merge classes

---
+++ src/org/spdx/merge/SpdxMergeException.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2014 Source Auditor Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+*/
+package org.spdx.merge;
+
+/**
+ * @author gling
+ *
+ */
+public class SpdxMergeException extends Exception{
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public SpdxMergeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public SpdxMergeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public SpdxMergeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
