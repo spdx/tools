4c8c837d7504a3f6dddd0ad8fa66125cef9a7bed
Gang Ling <linggang7@gmail.com>
2014-07-27 23:26:35 -0500

    change license head

--- src/org/spdx/merge/SpdxMergeException.java
+++ src/org/spdx/merge/SpdxMergeException.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014 Source Auditor Inc.
+ * Copyright (c) 2014 Gang Ling
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package org.spdx.merge;
 
 /**
- * @author gling
+ * @author Gang Ling
  *
  */
 public class SpdxMergeException extends Exception{
