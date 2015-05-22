d12e844f9e91e0ed690f82fc5b33b227d7f58d1d
gling07 <linggang7@gmail.com>
2015-05-22 16:47:50 -0500

    Apply code change to merge multiple packages.

--- src/org/spdx/merge/SpdxPackageInfoMerger.java
+++ src/org/spdx/merge/SpdxPackageInfoMerger.java
@@ -24,6 +24,7 @@ import java.util.List;
 import org.spdx.rdfparser.InvalidSPDXAnalysisException;
 import org.spdx.rdfparser.JavaSha1ChecksumGenerator;
 import org.spdx.rdfparser.license.AnyLicenseInfo;
+import org.spdx.rdfparser.license.ExtractedLicenseInfo;
 import org.spdx.rdfparser.license.LicenseInfoFactory;
 import org.spdx.rdfparser.model.SpdxDocument;
 import org.spdx.rdfparser.model.SpdxFile;
@@ -39,50 +40,81 @@ import org.spdx.spdxspreadsheet.InvalidLicenseStringException;
  */
 public class SpdxPackageInfoMerger {
 
+	    @Deprecated
 		private SpdxPackage packageInfoResult = null;
+	    @Deprecated
 		private SpdxDocument[] allDocs;
 		
 		private List<SpdxPackage> packagesResult = null;
 		private SpdxDocument[] subDocs = null;
+		private SpdxLicenseMapper mapper = null;
 		
-		/* 
+		/** 
 		 * @param masterPackagesInfo
 		 * @param subDocs
 		 */
-		public SpdxPackageInfoMerger(List<SpdxPackage> masterPackagesInfo, SpdxDocument[] subDocs){
+		public SpdxPackageInfoMerger(List<SpdxPackage> masterPackagesInfo, SpdxDocument[] subDocs
+				, SpdxLicenseMapper mapper){
 			this.packagesResult = masterPackagesInfo;
 			this.subDocs = subDocs;
+			this.mapper = mapper;
 		}
 		
-		/* A method to merge all packages' information from sub list documents into master document
+		/** A method to merge all packages' information from sub list documents into master document
 		 * @param subDocs
 		 * @param fileMergeResult
 		 * @return packagesResult
 		 * @throws InvalidSPDXAnalysisException
 		 */
 		public List <SpdxPackage> mergePackagesInfo(SpdxDocument[] subDocs, SpdxFile[] fileMergeResult)
-				throws InvalidSPDXAnalysisException{
+				throws InvalidSPDXAnalysisException, NoSuchAlgorithmException, InvalidLicenseStringException{
 			
 			List<SpdxPackage> retval = new ArrayList<SpdxPackage>(clonePackages(packagesResult));
 			
 			for(int i = 0; i < subDocs.length; i++){				
 				List<SpdxPackage> subPackagesInfo = subDocs[i].getDocumentContainer().findAllPackages();
-				SpdxPackage temp = null;
+				SpdxPackage tempPackage = null;
 				for(int p = 0; p < subPackagesInfo.size(); p++){
 					boolean foundNameMatch = false;
 					boolean foundSha1Match = false;
-	                temp = subPackagesInfo.get(p);
+					SpdxPackage masterPackage = null;
+					Integer index = null;
+	                tempPackage = subPackagesInfo.get(p);
 	                
 	                for(int q = 0; q < retval.size(); q++){
-	                	if(retval.get(q).getName().equalsIgnoreCase(temp.getName()))
+	                	if(retval.get(q).getName().equalsIgnoreCase(tempPackage.getName()))
 	                		foundNameMatch = true;
-	                	if(retval.get(q).getSha1().equals(temp.getSha1())){
+	                	if(retval.get(q).getSha1().equals(tempPackage.getSha1())){
 	                		foundSha1Match = true;
+	                		masterPackage = retval.get(q);
+	                		index = q;
 	                		break;
 	                	}
 	                }
 	                if(!foundNameMatch && !foundSha1Match){
-	                	retval.add(temp);
+	                	AnyLicenseInfo[] licFromFile = checkLicenseFromFile(subDocs[i],tempPackage);
+	                	tempPackage.setLicenseInfosFromFiles(licFromFile);
+	                	retval.add(tempPackage);
+	                }
+	                else{
+	                	 AnyLicenseInfo[] masterLicFromFile = masterPackage.getLicenseInfoFromFiles();
+	                	 AnyLicenseInfo[] licFromFile = checkLicenseFromFile(subDocs[i],tempPackage);
+	                	 ArrayList <AnyLicenseInfo> licList = new ArrayList<AnyLicenseInfo> (Arrays.asList(masterLicFromFile));
+	                	 for(int g = 0; g < licFromFile.length; g++){
+	                		 for(int d = 0; d < licList.size(); d++){
+	                			 if(licList.get(d).equals(licFromFile[g]))
+	                				 break;
+	                			 else
+	                				 licList.add(licFromFile[g]);
+	                		 }
+	                	 }
+	                	 AnyLicenseInfo[] mergedLicFromFile = licList.toArray(new AnyLicenseInfo[licList.size()]);
+	                	 masterPackage.setLicenseInfosFromFiles(mergedLicFromFile);
+	                	 String[] skippedFiles = collectSkippedFiles(masterPackage,tempPackage);
+	                	 VerificationCodeGenerator verCodeGenerator = new VerificationCodeGenerator(new JavaSha1ChecksumGenerator());
+	                	 SpdxPackageVerificationCode newCode = verCodeGenerator.generatePackageVerificationCode(fileMergeResult, skippedFiles);
+	                	 masterPackage.setPackageVerificationCode(newCode);
+	                	 retval.set(index, masterPackage);
 	                }
 	                
 				}
@@ -90,10 +122,27 @@ public class SpdxPackageInfoMerger {
 			}
 			return packagesResult;
 		}
-
+        
+	    /** A method to check license from file information through mapLicenseInfo function. If it is a
+	     * non-standard license, check this particular license in the Mapper class. Replace the current 
+	     * license value with the return value from the mapNonStdLicInMap method. After all, all the declared
+	     * license should remain the same and non-standard license should replaced with updated id
+	     * @param doc
+	     * @param temp
+	     * @return licFromFile
+	     */
+		public AnyLicenseInfo[] checkLicenseFromFile(SpdxDocument doc, SpdxPackage temp){
+			AnyLicenseInfo[] licFromFile = temp.getLicenseInfoFromFiles();
+			for(int k = 0; k < licFromFile.length; k++){
+				if(licFromFile[k] instanceof ExtractedLicenseInfo){
+					AnyLicenseInfo tempLicense = mapper.mapNonStdLicInMap(doc, licFromFile[k]);
+					licFromFile[k] = tempLicense;
+				}
+			}			
+			return licFromFile;
+		}
 		
-		/**
-		 * 
+		/** A method to clone the packages in the list 
 		 * @param packagesArray
 		 * @return clonedPackagesArray
 		 */
@@ -118,14 +167,6 @@ public class SpdxPackageInfoMerger {
 		public SpdxPackage mergePackageInfo(SpdxDocument[] subDocs, SpdxFile[] fileMergeResult) 
 				throws InvalidSPDXAnalysisException, NoSuchAlgorithmException, InvalidLicenseStringException{
 			
-			String[] skippedFiles = collectSkippedFiles();
-			VerificationCodeGenerator vg = new VerificationCodeGenerator(new JavaSha1ChecksumGenerator());
-			SpdxPackageVerificationCode result = vg.generatePackageVerificationCode(fileMergeResult, skippedFiles);
-			packageInfoResult.setPackageVerificationCode(result);
-			
-			AnyLicenseInfo[] licsInFile = collectLicsInFiles(fileMergeResult);
-			packageInfoResult.setLicenseInfosFromFiles(licsInFile);
-			
 			AnyLicenseInfo declaredLicense = LicenseInfoFactory.parseSPDXLicenseString("NOASSERTION");
 			packageInfoResult.setLicenseDeclared(declaredLicense);		
 			
@@ -135,23 +176,27 @@ public class SpdxPackageInfoMerger {
 			return packageInfoResult;			
 		}
 		
-		@Deprecated
 		/**
-		 * method to collect all skipped files from input SPDX documents
-		 * @return
+		 * A method to collect all skipped files from input SPDX package.
+		 * @return excludedFileNamesArray
 		 * @throws InvalidSPDXAnalysisException
 		 */
-		public String[] collectSkippedFiles() throws InvalidSPDXAnalysisException{
+		public String[] collectSkippedFiles(SpdxPackage main, SpdxPackage sub ) throws InvalidSPDXAnalysisException{
 			ArrayList<String> excludedFileNamesList = new ArrayList<String>();
-			for(int p = 0; p < allDocs.length; p++){
-			  List<SpdxPackage> packageList = allDocs[p].getDocumentContainer().findAllPackages();
-			  for(int h = 0; h < packageList.size(); h++){
-				String[] retval = packageList.get(h).getPackageVerificationCode().getExcludedFileNames();
-				if(excludedFileNamesList.size() == 0){
-					for(int i = 0; i < retval.length; i++){
-						excludedFileNamesList.add(i, retval[i]);
-					}
-				}else{
+			String[] retval = sub.getPackageVerificationCode().getExcludedFileNames();
+			String[] skippedFileInMain = main.getPackageVerificationCode().getExcludedFileNames();
+			
+			if(skippedFileInMain.length == 0 && retval.length == 0){
+				String[] excludedFileNamesArray = new String[0];
+				return excludedFileNamesArray;
+			}
+			else if(skippedFileInMain.length != 0){
+				excludedFileNamesList = new ArrayList<String>(Arrays.asList(skippedFileInMain));
+			}
+			else if(excludedFileNamesList.isEmpty() && retval.length != 0){
+				excludedFileNamesList = new ArrayList<String>(Arrays.asList(retval));
+			}
+			else{
 					for(int k = 0; k < retval.length; k++){
 						boolean foundNameMatch = false;
 						for(int q = 0; q < excludedFileNamesList.size(); q++){
@@ -164,47 +209,12 @@ public class SpdxPackageInfoMerger {
 						}
 					}
 				}
-			  }
-			}
 			String[] excludedFileNamesArray = new String[excludedFileNamesList.size()];
 			excludedFileNamesList.toArray(excludedFileNamesArray);
 			excludedFileNamesList.clear();
 			return excludedFileNamesArray;
 		}
-		@Deprecated
-		/**
-		 * method to collect all license information from file merging result
-		 * @param fileMergeResult
-		 * @return
-		 */
-		public AnyLicenseInfo[] collectLicsInFiles(SpdxFile[] fileMergeResult){
-			ArrayList<AnyLicenseInfo> licsList = new ArrayList<AnyLicenseInfo>();
-			for(int a = 0; a < fileMergeResult.length; a++){
-				AnyLicenseInfo[] retval = fileMergeResult[a].getLicenseInfoFromFiles();
-				if(licsList.size() == 0){
-					for(int b = 0; b < retval.length; b++){
-						licsList.add(b, retval[b]);
-					}
-				}else{
-					for(int c = 0; c < retval.length; c++){
-						boolean foundLicMatch = false;
-						for(int d = 0; d < licsList.size(); d++){
-							if(retval[c].equals(licsList.get(d))){
-								foundLicMatch = true;
-								break;
-							}
-						}
-						if(!foundLicMatch){
-							licsList.add(retval[c]);
-						}
-					}
-				}
-			}
-			AnyLicenseInfo[] licsInFile = new AnyLicenseInfo[licsList.size()];
-			licsList.toArray(licsInFile);
-			licsList.clear();
-			return licsInFile;	
-		}
+
 		@Deprecated
 		/**
 		 * 
