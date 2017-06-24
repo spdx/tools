01150041e0688f8543ef2ba191fd0fb46c4af55c
Gary O'Neall <gary@sourceauditor.com>
2017-06-24 12:40:23 -0700

    Refactor verify tool so that a static method verify(String filepath) can be called by external utilities
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

---
+++ src/org/spdx/tools/SpdxVerificationException.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package org.spdx.tools;
+
+/**
+ * @author Gary O'Neall
+ *
+ */
+public class SpdxVerificationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @param msg
+	 */
+	public SpdxVerificationException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * @param inner
+	 */
+	public SpdxVerificationException(Throwable inner) {
+		super(inner);
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public SpdxVerificationException(String msg, Throwable inner) {
+		super(msg, inner);
+	}
+
+}
