33406324fee2e51e7e8943292ccd5314ac514e28
rtgdk <rohit.lodhartg@gmail.com>
2017-07-20 20:49:35 +0530

    Identify Invalid SPDX File format and Invalid Tag Value file as different error.
    	*Add a new Exception InvalidFileFormatException for all invalid spdx supported file formats.
    	*Pass on correct error to enclosing function to identify invalid file or invalid tag value file or rdf file.
    
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

---
+++ src/org/spdx/tag/InvalidFileFormatException.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.spdx.tag;
+
+/**
+ * @author rtgdk
+ *
+ */
+public class InvalidFileFormatException extends Exception {
+
+	/**
+	 * 
+	 */
+	public InvalidFileFormatException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public InvalidFileFormatException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public InvalidFileFormatException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public InvalidFileFormatException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public InvalidFileFormatException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
