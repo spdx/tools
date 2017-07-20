0725b8e68f1c49d29895fc538e8b3df7dae7d8f3
rtgdk <rohit.lodhartg@gmail.com>
2017-07-20 22:41:56 +0530

    Add documentation to InvalidFileFormatException
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- src/org/spdx/tag/InvalidFileFormatException.java
+++ src/org/spdx/tag/InvalidFileFormatException.java
@@ -1,10 +1,25 @@
 /**
- * 
- */
+ * Copyright (c) 2017 Source Auditor Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+*/
 package org.spdx.tag;
 
 /**
- * @author rtgdk
+ * Exceptions for invalid SPDX file format
+ * 
+ * @author Rohit Lodha
  *
  */
 public class InvalidFileFormatException extends Exception {
@@ -13,7 +28,6 @@ public class InvalidFileFormatException extends Exception {
 	 * 
 	 */
 	public InvalidFileFormatException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -21,7 +35,6 @@ public class InvalidFileFormatException extends Exception {
 	 */
 	public InvalidFileFormatException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -29,7 +42,6 @@ public class InvalidFileFormatException extends Exception {
 	 */
 	public InvalidFileFormatException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -38,7 +50,6 @@ public class InvalidFileFormatException extends Exception {
 	 */
 	public InvalidFileFormatException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -50,7 +61,6 @@ public class InvalidFileFormatException extends Exception {
 	public InvalidFileFormatException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
