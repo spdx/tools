ae2f649769565c692fcc706ad14371e7e199f288
Gary O'Neall <gary@sourceauditor.com>
2018-01-06 14:40:31 -0800

    Add properties to force the FsfFreeParse to use local files and allow UNKNOWN for DOAPProject home pages
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/org/spdx/tools/licensegenerator/FsfLicenseDataParser.java
+++ src/org/spdx/tools/licensegenerator/FsfLicenseDataParser.java
@@ -39,6 +39,16 @@ import com.google.common.collect.Maps;
 /**
  * Singleton class which returns information maintained by the Free Software Foundation
  * 
+ * The default behavior is to pull the FSF data from <code>https://wking.github.io/fsf-api/licenses-full.json</code>
+ * 
+ * If the URL is not accessible, the file resources/licenses-full.json in the same path as the .jar file will be used.
+ * If the local file can not be found, then a properties file resources/licenses-full.json will be used.
+ * 
+ * There are two properties that can be used to control where the JSON file is loaded from:
+ *   LocalFsfFreeJson - if set to true, then use the local file or, if the local file is not found, the resource files and don't access the file from the github.io page
+ *   FsfFreeJsonUrl - the URL to pull the JSON file from.  If both LocalFsfFreeJson and FsfFreeJsonUrl are specified, then
+ *   the LocalFsfFreeJson takes precedence and the local resource file will be used.
+ * 
  * NOTE: This is currently using a non authoritative data source
  * 
  * TODO: Update the class to use an official FSF data source once available
@@ -47,25 +57,34 @@ import com.google.common.collect.Maps;
  */
 public class FsfLicenseDataParser {
 	
-	static final String FSF_JSON_URL = "https://wking.github.io/fsf-api/licenses-full.json";
+	static final String PROP_USE_ONLY_LOCAL_FILE = "LocalFsfFreeJson";
+	static final String PROP_FSF_FREE_JSON_URL = "FsfFreeJsonUrl";
+	
+	static final String DEFAULT_FSF_JSON_URL = "https://wking.github.io/fsf-api/licenses-full.json";
 	static final String FSF_JSON_FILE_PATH = "resources" + File.separator + "licenses-full.json";
 	static final String FSF_JSON_CLASS_PATH = "resources/licenses-full.json";
 	
 	private static FsfLicenseDataParser fsfLicenseDataParser = null;
 	private Map<String, Boolean> licenseIdToFsfFree;
+	private boolean useOnlyLocalFile = false;
+	private String licenseJsonUrl = DEFAULT_FSF_JSON_URL;
 	
 	private FsfLicenseDataParser() throws LicenseGeneratorException {
 		licenseIdToFsfFree = Maps.newHashMap();
+		useOnlyLocalFile = Boolean.parseBoolean(System.getProperty(PROP_USE_ONLY_LOCAL_FILE, "false"));
+		licenseJsonUrl = System.getProperty(PROP_FSF_FREE_JSON_URL, DEFAULT_FSF_JSON_URL);
 		Reader reader = null;
 		try {
-			// First, try the URL
-			try {
-				URL url = new URL(FSF_JSON_URL);
-				reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			} catch (MalformedURLException e) {
-				reader = null;
-			} catch (IOException e) {
-				reader = null;
+			if (!useOnlyLocalFile) {
+				// First, try the URL
+				try {
+					URL url = new URL(licenseJsonUrl);
+					reader = new BufferedReader(new InputStreamReader(url.openStream()));
+				} catch (MalformedURLException e) {
+					reader = null;
+				} catch (IOException e) {
+					reader = null;
+				}
 			}
 			if (reader == null) {
 				// try the file system
